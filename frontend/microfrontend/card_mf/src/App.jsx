e83816d6a13708efee99b7d9f0926db1b796dec3
Ilya Bobrov <ilya.bobrov@bscmsc.ru>
2025-01-01 20:40:08 +0300

    Задание 1 спринта

---
+++ frontend/microfrontend/card_mf/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import api from "./utils/api.js";
+
+function App() {
+
+  function handleAddPlaceSubmit(newCard) {
+    api
+      .addCard(newCard)
+      .then((newCardFull) => {
+        setCards([newCardFull, ...cards]);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err));
+  }
+}
